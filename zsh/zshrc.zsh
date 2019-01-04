3924df3fe3c0195a75c691c77b6d033cd3d814dc
0xazure <stuart.crust@gmail.com>
2019-01-04 16:05:47 -0500

    sh: update zsh configuration

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,65 +1,100 @@
-# Path to oh-my-zsh installation
-export ZSH=$HOME/.oh-my-zsh
-# Path to custom oh-my-zsh tweaks
-export ZSH_CUSTOM=$DOT/zsh/custom
-# Disable oh-my-zsh auto update
-DISABLE_AUTO_UPDATE=true
-# Display red dots while waiting for completion
-COMPLETION_WAITING_DOTS="true"
-# Disable marking untracked files as dirty
-DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Plugins loaded from ~/.oh-my-zsh/plugins/*
-plugins=(
-brew
-brew-cask
-docker
-docker-compose
-git
-heroku
-rails
-tmux
-)
-
-# Set name of the prompt theme to load
-ZSH_THEME="pure"
-
-# oh-my-zsh
-[[ -s $ZSH/oh-my-zsh.sh ]] && . $ZSH/oh-my-zsh.sh
-
-# Load aliases
-[[ -s $DOT/zsh/aliases.zsh ]] && . $DOT/zsh/aliases.zsh
-
-# Ask before executing `rm folder/*`
-unsetopt RM_STAR_SILENT
-# Don't store commands prefixed with a space in history
-set HIST_IGNORE_SPACE
-
-# tmux
-## Don't autoset pane title
-export DISABLE_AUTO_TITLE=true
-
-# golang
-export GOPATH=$HOME/golang
-mkdir -p $GOPATH
-export PATH=$GOPATH/bin:$PATH
-
-# rust
-export PATH=$HOME/.cargo/bin:$PATH
-
-# rbenv
-eval "$(rbenv init -)"
-
-# pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-eval "$(pyenv init -)"
 
-# virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/devel
-[[ -s /usr/local/bin/virtualenvwrapper.sh ]] && . /usr/local/bin/virtualenvwrapper.sh
+export RBENV_ROOT="$HOME/.rbenv"
 
-# Source shell syntax highlighting
+PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+PATH="$HOME/.cargo/bin:$PATH"
+PATH="$PYENV_ROOT/bin:$PATH"
+
+export PATH
+
+fpath=( "$HOME/.dotfiles/zsh/zfunctions" $fpath )
+
+# Use prompt sindresorhus/pure
+autoload -U promptinit && promptinit
+prompt pure
+
+# Initialize completion system
+autoload -U compinit && compinit
+
+# Start menu completion after second consecutive <TAB>
+setopt auto_menu
+
+# Enable interactive (cursor-keys) menu
+zstyle ':completion:*:*:*:*:*' menu select
+
+# Use completion caching
+zstyle ':completion::complete:*' use-cache 1
+
+# Case-insensitive, hypen-insensitive completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
+
+HISTFILE=$HOME/.zsh_history
+
+HISTSIZE=10000
+SAVEHIST=10000
+
+# History timestamp format yyyy-mm-dd
+alias history='fc -il 1'
+
+# Append to, rather than replace, history file
+setopt append_history
+
+# Save command timestamp and duration in $HISTFILE
+setopt extended_history
+
+setopt hist_expire_dups_first
+
+# Ignore duplicates in command history list
+setopt hist_ignore_dups
+
+# Do not save commands beginning with a space in $HISTFILE
+setopt hist_ignore_space
+
+# Perform history expansion and reload line into editing buffer before execution
+setopt hist_verify
+
+# Imports new commands from $HISTFILE, as well as appends typed commands as though
+# ING_APPEND_HISTORY were set
+setopt share_history
+
+ if which pyenv > /dev/null; then
+	eval "$(pyenv init -)"
+	pyenv virtualenvwrapper_lazy
+ fi
+
+alias docdel='docker rm $(docker ps --all --quiet --filter status=exited)'
+
+alias ag='ag --path-to-ignore ~/.ignore'
+
+alias g='git'
+alias ga='git add'
+alias gc='git commit --verbose'
+alias gc!='git commit --verbose --amend'
+alias gco='git checkout'
+alias gst='git status'
+
+take () {
+	mkdir -p $1
+	cd $1
+}
+
+# tree
+function t() {
+	tree -I '.git|node_modules|vendor|.DS_Store' --dirsfirst --filelimit 15 -L ${1:-3} -aC $2
+}
+
+# Source zsh syntax highlighting
 if [[ -s /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
-  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+	source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+# Source zsh history substring search
+if [[ -s ~/.dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh ]]; then
+	source ~/.dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+	# <UP> to search backwards in history
+	bindkey '^[[A' history-substring-search-up
+	# <DOWN> to search forwards in history
+	bindkey '^[[B' history-substring-search-down
 fi
