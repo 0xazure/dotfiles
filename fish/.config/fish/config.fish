66c1ab01c1c892ec3269828cd8bf51e4ee558de4
0xazure <stuart.crust@gmail.com>
2019-01-08 20:12:16 -0500

    sh: clippy is no longer nightly-only

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -24,8 +24,6 @@ if status --is-interactive
 		abbr --add la ls -la
 	end
 
-	abbr --add clippy cargo +nightly clippy
-
 	set --export PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 	set --export PATH /Users/azure/.cargo/bin $PATH
 end
